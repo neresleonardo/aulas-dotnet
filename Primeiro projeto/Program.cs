49194d5301b5803274896d92d312847b5d9542e6
Leonardo Borges <leonardo.borges.neres@gmail.com>
2023-11-30 19:23:06 -0300

    aulas MapGet,MapPost,MapDelete

--- Primeiro projeto/Program.cs
+++ Primeiro projeto/Program.cs
@@ -2,5 +2,12 @@
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World 5!");
+app.MapPost("/user", () => new { Name = "Leonardo", Age = 25 });
+app.MapGet("/AddHeader", (HttpResponse response) => response.Headers.Add("Teste", "Leonardo"));
+
+app.MapGet("/AddHeader1", (HttpResponse response) => {
+    response.Headers.Add("Teste", "Leonardo");
+    return "Leonardo";
+});
 
 app.Run();
